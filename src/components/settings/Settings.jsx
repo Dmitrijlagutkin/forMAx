c544e02a9e80aaf35a7501009d7119e4542180cb
Dmitrij Lagutkin <dmitrijlagutkin@gmail.com>
2021-03-26 06:26:03 +0100

    added settings panel

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,39 +1,144 @@
 import { Drawer, IconButton, TextField } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import ListIcon from '@material-ui/icons/List';
+import Button from '@material-ui/core/Button';
+import ShareIcon from '@material-ui/icons/Share';
+import SecurityIcon from '@material-ui/icons/Security';
+import Switches from '../common/Switch'
+import AssignmentIcon from '@material-ui/icons/Assignment';
 import useStyles from "./stylesSettings"
+
 
 export default function Settings({isOpenSettings, onClickButtonSettingsHandler}) {
     const classes = useStyles()
 
     return(
         <Drawer anchor="right" open={isOpenSettings} className={classes.settingsContainer}>
-            <div className={`${classes.settingsItem} ${classes.settingsTop}`}>
-                <IconButton onClick={onClickButtonSettingsHandler} className={classes.closeButton}>
-                    <CloseIcon />
+            <div className={`${classes.settingsItem} ${classes.settingsOther}`}>
+                <IconButton onClick={onClickButtonSettingsHandler} >
+                    <CloseIcon className={classes.closeButton}/>
                 </IconButton>
                 <p className={classes.settingsTopTitle}>
                     This Clip Pad Settings
                 </p>
             </div>
             <div className={classes.settingsItem}>
-                <p> Clip pad name </p>
-                <TextField
-                className={classes.settingsInput}
-                label="Clip pad name"
-                variant="outlined"
-                />
+                <p className={classes.settingsTitle}> Clip pad name </p>
+                <div className={classes.settingsOther}>
+                    {/* <p className={classes.settingsName}>Name</p> */}
+                    <IconButton className={classes.iconEmail}>
+                        <AssignmentIcon />
+                    </IconButton>
+                    <TextField
+                    className={classes.settingsInput}
+                    label="Clip pad name"
+                    variant="outlined"
+                    size="small"
+                    />
+                </div>
+                
             </div>
+
             <div className={classes.settingsItem}>
-                <p> Email settings for thiss clip pad </p>
-                <TextField
-                className={classes.settingsInput}
-                label="Email"
-                variant="outlined"
-                />
+                <p className={classes.settingsTitle}> Email settings for thiss clip pad </p>
+                <div className={classes.settingsOther}>
+                    <IconButton className={classes.iconEmail}>
+                        <MailOutlineIcon />
+                    </IconButton>
+                    <TextField
+                    className={classes.settingsInput}
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    />
+                </div>
             </div>
+
             <div className={classes.settingsItem}>
-                <p> Add user for this clip pad </p>
+                <div className={classes.settingsFlex}> 
+                    <IconButton className={classes.iconAddUser}>
+                        <GroupAddIcon />
+                    </IconButton>
+                    <p className={classes.titleAddUser}> Add user for this clip pad </p>
+                </div>
+                <div className={classes.settingsOther}>
+                    <div className={classes.iconPlusMinus}>
+                        <IconButton >
+                            <AddCircleIcon className={classes.iconAddUser}/>
+                        </IconButton>
+                        <IconButton >
+                            <RemoveCircleIcon className={classes.iconAddUser}/>
+                        </IconButton>
+                    </div>
+                    <TextField
+                    className={classes.settingsInput}
+                    label="Add user email"
+                    variant="outlined"
+                    size="small"
+                    />
+                    <p className={classes.userStatus}>status - </p>
+                    <p className={classes.userStatus}>joined </p>
+                </div>
+                <div className={classes.settingsOther}>
+                    <div className={classes.iconPlusMinus}>
+                        <IconButton >
+                            <AddCircleIcon className={classes.iconAddUser}/>
+                        </IconButton>
+                        <IconButton >
+                            <RemoveCircleIcon className={classes.iconAddUser}/>
+                        </IconButton>
+                    </div>
+                    <TextField
+                    className={classes.settingsInput}
+                    label="Add user email"
+                    variant="outlined"
+                    size="small"
+                    />
+                    <p className={classes.userStatus}>status - </p>
+                    <p className={classes.userStatus}>invited </p>
+                </div>
+                <Button className={classes.buttonSend} variant="contained" color="primary">
+                    Send
+                </Button>
             </div>
+
+            <div>
+                <div className={`${classes.settingsFlex} ${classes.borderBottom}`}> 
+                    <IconButton className={classes.iconAddUser}>
+                        <ListIcon />
+                    </IconButton>
+                    <p className={classes.titleAddUser}> See all your clip pads</p>
+                </div>
+            </div>
+
+            <div className={classes.borderBottom}>           
+                <div className={classes.settingsFlex}> 
+                    <IconButton className={classes.iconAddUser}>
+                        <SecurityIcon />
+                    </IconButton>
+                    
+                    <p className={classes.titleAddUser}>Privaci settings</p>
+                </div>
+                <div className={classes.settingsFlexCenter}> 
+                    <p className={classes.privaceText}>Privace</p>
+                    <Switches/>
+                    <p className={classes.privaceText}>Pablic</p>
+                </div>
+            </div>
+
+            <div>
+                <div className={classes.settingsFlex}> 
+                    <IconButton className={classes.iconAddUser}>
+                        <ShareIcon />
+                    </IconButton>
+                    <p className={classes.titleAddUser}> Share</p>
+                </div>
+            </div>
+            
         </Drawer>
     )
 }
--- src/components/settings/stylesSettings.js
+++ src/components/settings/stylesSettings.js
@@ -2,35 +2,80 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme)=>({
     settingsContainer: {
-        fontFamily: "Roboto"
+        fontFamily: "Roboto",
+        fontSize: '14px',
     },
     settingsItem: {
-       padding: "20px 20px 30px",
-       borderBottom: "1px solid #2c79c5"
+       padding: "14px 20px 14px",
+       borderBottom: "1px solid #2c79c5",
+       width: '410px'
     },
-    settingsTop: {
-        position: "relaitive"
+    settingsOther: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    settingsFlex: {
+        display: 'flex',
     },
     closeButton: {
-        position: "absolute",
-        top: "0px",
-        left: "0px"
+        fontSize: 'medium',
+        color: '#000',
     },
     settingsTopTitle: {
-        textAlign: "end",
-        marginTop: "5px",
+        paddingRight: '10px',
+        marginTop: "10px",
         fontWeight: "500"
+    },
+    settingsName: {
+        marginTop: '10px',
+        color: '#2c79c5',
     },
     settingsInput: {
         '& label.Mui-focused': {
             color: '#2c79c5',
+            
           },
           '& .MuiOutlinedInput-root': {
             '&.Mui-focused fieldset': {
               borderColor: '#2c79c5',
+
             },
           },
-    }
+    },
+    settingsTitle: {
+        marginBottom: '10px',
+    },
+    iconEmail: {
+        color: '#2c79c5'
+    },
+    iconAddUser: {
+        marginTop: '0',
+       color: '#2c79c5',
+    },
+    titleAddUser: {
+        marginTop: '15px',
+    },
+    iconPlusMinus: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+    },
+    userStatus: {
+        marginTop: '10px',
+    },
+    buttonSend: {
+        display: 'block',
+        margin: '0 auto',
+    },
+    borderBottom: {
+        borderBottom: "1px solid #2c79c5",
+    },
+    settingsFlexCenter: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    privaceText: {
+        marginTop: '10px',
+    },
 }))
 
 export default useStyles
